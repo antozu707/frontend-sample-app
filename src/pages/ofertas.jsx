f561d7f6b0c25ab90b6704aeb9a413b0fc688bc5
antozu707 <antonia.zuniga@usm.cl>
2024-11-04 14:44:35 -0300

    nuevo commit en ofertas

--- src/pages/ofertas.jsx
+++ src/pages/ofertas.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 
 function Ofertas() {
   return (
-
-    <main style={{ width: '75%'}}>
-
-
-
-        <div style={{ border: '2px solid #ff6600', borderRadius: '10px', padding: '15px', marginBottom: '20px', margin: '0 auto', width: '500px' }}>
+    <div>
+        <div style={{ border: '2px solid #ff6600', borderRadius: '10px', padding: '15px', marginBottom: '30px', margin: '0 auto', width: '500px' }}>
             <p><strong>Empresa:</strong> Banco de Chile</p>
             <p><strong>Ubicación:</strong> Santiago, Chile</p>
             <p><strong>Detalles:</strong> Práctica Data Scientist, modalidad presencial</p>
@@ -16,16 +12,16 @@ function Ofertas() {
                 </button>
         </div>
 
-        <div style={{ border: '2px solid #ff6600', borderRadius: '10px', padding: '15px', marginBottom: '20px', margin: '0 auto', width: '500px' }}>
-        <p><strong>Empresa:</strong> Banco de Chile</p>
-        <p><strong>Ubicación:</strong> Santiago, Chile</p>
-        <p><strong>Detalles:</strong> Práctica Data Scientist, modalidad presencial</p>
-            <button style={{ backgroundColor: '#ff6600', color: 'white', border: 'none', padding: '5px 10px', cursor: 'pointer', borderRadius: '5px' }}>
-            Postular
-            </button>
+        <div style={{ border: '2px solid #ff6600', borderRadius: '10px', padding: '15px', marginBottom: '30px', margin: '0 auto', width: '500px' }}>
+            <p><strong>Empresa:</strong> Samsung</p>
+            <p><strong>Ubicación:</strong> Santiago, Chile</p>
+            <p><strong>Detalles:</strong> Práctica Desarrollador Software, modalidad presencial</p>
+                <button style={{ backgroundColor: '#ff6600', color: 'white', border: 'none', padding: '5px 10px', cursor: 'pointer', borderRadius: '5px' }}>
+                Postular
+                </button>
         </div>
 
-    </main>
+    </div>
 
   );
 }
