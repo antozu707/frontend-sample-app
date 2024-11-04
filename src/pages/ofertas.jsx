1fe829112c517520a1d83472b1491e5e216167fa
antozu707 <antonia.zuniga@usm.cl>
2024-11-04 17:32:41 -0300

    cambios en ofertas

--- src/pages/ofertas.jsx
+++ src/pages/ofertas.jsx
@@ -21,6 +21,24 @@ function Ofertas() {
                 </button>
         </div>
 
+        <div style={{ border: '2px solid #ff6600', borderRadius: '10px', padding: '15px', marginBottom: '30px', margin: '0 auto', width: '500px' }}>
+            <p><strong>Empresa:</strong> Cencosud</p>
+            <p><strong>Ubicación:</strong> Concepción, Chile</p>
+            <p><strong>Detalles:</strong> Práctica Backend Developer, modalidad híbrida</p>
+                <button style={{ backgroundColor: '#ff6600', color: 'white', border: 'none', padding: '5px 10px', cursor: 'pointer', borderRadius: '5px' }}>
+                Postular
+                </button>
+        </div>
+
+        <div style={{ border: '2px solid #ff6600', borderRadius: '10px', padding: '15px', marginBottom: '30px', margin: '0 auto', width: '500px' }}>
+            <p><strong>Empresa:</strong> Uber</p>
+            <p><strong>Ubicación:</strong> Santiago, Chile</p>
+            <p><strong>Detalles:</strong> Práctica Ciberseguridad, modalidad remota</p>
+                <button style={{ backgroundColor: '#ff6600', color: 'white', border: 'none', padding: '5px 10px', cursor: 'pointer', borderRadius: '5px' }}>
+                Postular
+                </button>
+        </div>
+
     </div>
 
   );
